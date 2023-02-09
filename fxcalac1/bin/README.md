7b49f02035003297754e4e34bf4c7022677798c8
Abdessamad <abdessamadmymail@gmail.com>
2023-02-09 20:44:08 +0100

    first functional

---
+++ fxcalac1/bin/README.md
@@ -0,0 +1,4 @@
+This is a calculator 
+i used the javaluator : https://javaluator.sourceforge.net/en/home/
+i shoukd fix the problem of big integers that doesn't appear on the screen or make the screen slide
+i should also add some styling and other fuctions
---
+++ fxcalac1/src/controller.java
@@ -0,0 +1,78 @@
+import javafx.fxml.FXML;
+// import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+// import javafx.fxml.FXMLLoader;
+
+// import java.io.IOException;
+
+// import javax.script.ScriptEngine;
+// import javax.script.ScriptEngineManager;
+// import javax.script.ScriptException;
+
+import com.fathzer.soft.javaluator.DoubleEvaluator;
+
+import javafx.event.*;
+
+public class controller {
+    // @FXML
+    // ScriptEngineManager manager = new ScriptEngineManager();
+    // ScriptEngine engine = manager.getEngineByName("js");
+    DoubleEvaluator evaluator = new DoubleEvaluator();
+    @FXML
+    private Stage stage ;
+    
+    @FXML
+    private Label lbl;
+    
+    private String s="";
+
+
+    
+
+    // public void calc(ActionEvent event ) throws IOException{
+    //     root =FXMLLoader.load(getClass().getResource("calculator1.fxml"));
+    //     stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+    //     scene=new Scene(root);
+    //     stage.setScene(scene);
+    //     stage.show();
+ 
+    public void action(ActionEvent event){
+        Button btn=(Button)event.getSource();
+        String id=btn.getId();
+        if (id.contentEquals("del")){
+            try {
+                s=s.substring(0, s.length()-1);
+                lbl.setText(s);
+            } catch (Exception e) {
+                // TODO: handle exception
+                //Nothing to do
+            }
+
+        }
+        else if (id.contentEquals("eq")){
+            try {
+                Double f=evaluator.evaluate(s);
+                s=Double.toString(f);
+                lbl.setText(s);
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                lbl.setText("Error");
+            }
+            
+        }
+        else{
+            s+=btn.getText() ;System.out.println(s);
+            lbl.setText(s);
+        }
+        
+    
+
+}
+    
+
+    
+}
